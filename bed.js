1b93ca7541e1b2443e2c35da4983745d4eab5b6f
gowsan2010 <77194511+gowsan2010@users.noreply.github.com>
2021-07-17 16:10:43 +0300

    Add files via upload

--- bed.js
+++ bed.js
@@ -1,7 +1,7 @@
 var status="";
 objects=[];
 function preload(){
-img = loadImage('electronics.jpeg');
+img = loadImage('Bed.jpg');
 }
 function setup(){
     canvas = createCanvas(600, 600);
@@ -16,11 +16,11 @@ if(status != ""){
     for (i=0; i<objects.length; i++){
         document.getElementById("status").innerHTML="Objects Detected";
 
-        fill("#FF0000");
+        fill("#FFFFFF");
         percent = floor(objects[i].confidence*100);
         text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
         noFill();
-        stroke("#FF0000");
+        stroke("#FFFFFF");
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
 }
--- stationary.js
+++ stationary.js
@@ -1,7 +1,7 @@
 var status="";
 objects=[];
 function preload(){
-img = loadImage('electronics.jpeg');
+img = loadImage('stationary.jpg');
 }
 function setup(){
     canvas = createCanvas(600, 600);
@@ -16,11 +16,11 @@ if(status != ""){
     for (i=0; i<objects.length; i++){
         document.getElementById("status").innerHTML="Objects Detected";
 
-        fill("#FF0000");
+        fill("#000000");
         percent = floor(objects[i].confidence*100);
         text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
         noFill();
-        stroke("#FF0000");
+        stroke("#000000");
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
 }
